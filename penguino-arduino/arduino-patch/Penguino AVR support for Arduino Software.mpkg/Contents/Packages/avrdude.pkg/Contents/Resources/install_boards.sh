33b0cda16c4b6e8f29b6b2c18fe1ed3e00da3243
David Collien <davidcollien@gmail.com>
2010-08-15 12:48:14 +0000

    dont want this

--- penguino-arduino/arduino-patch/Penguino AVR support for Arduino Software.mpkg/Contents/Packages/avrdude.pkg/Contents/Resources/install_boards.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino
-cat boards_addition.txt boards.txt > new_boards.txt
-mv new_boards.txt boards.txt
-rm boards_addition.txt
--- penguino-arduino/arduino-patch/Penguino AVR support for Arduino Software.mpkg/Contents/Packages/boardsaddition.pkg/Contents/Resources/install_avrdude.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin
-mv avrdude avrdude-real
